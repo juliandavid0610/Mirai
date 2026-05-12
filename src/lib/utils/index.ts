b0f1e15dcce4adc100918d19fc4d30d9c6cf9fac
juliandavid0610 <daisukenagata0209@gmail.com>
2026-05-12 13:40:04 +0900

    Re-export the utilities

---
+++ src/lib/utils/index.ts
@@ -0,0 +1,14 @@
+export { cn } from './cn';
+export { createId, slugify } from './id';
+export { createLogger, type Logger } from './logger';
+export {
+  clamp,
+  clamp01,
+  damp,
+  inverseLerp,
+  lerp,
+  mulberry32,
+  remap,
+  rms,
+} from './math';
+export { STORAGE_KEYS, readJSON, remove, writeJSON } from './storage';
